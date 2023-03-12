7f4690a64a5248502ea3fa58beefa55c6d9f82a4
Osmar Peña <osmarpena@infoutilitygt.com>
2023-03-12 17:41:24 -0600

    eliminarTemp,eliminarModelo, mitad de cargarModelo

--- Proyecto1/controllers/controllers.js
+++ Proyecto1/controllers/controllers.js
@@ -122,17 +122,82 @@ exports.cons10 = async function(req,res){
 
 exports.deleteTemp = async function(req,res){
     try{
-        res.status(200).send({msg:"eliminarTemporal", valid:true})
+        console.log("a")
+        var connection = await oracledb.getConnection({
+            user: user,
+            password: pass,
+            connectString: conn
+        });
+        
+
+        await connection.execute(`
+            DROP TABLE temporal`);
+        await connection.close()
+        var connection = await oracledb.getConnection({
+            user: user,
+            password: pass,
+            connectString: conn
+        });
+        await connection.execute(`
+        CREATE TABLE temporal(
+            NOMBRE_VICTIMA VARCHAR2(150),
+            APELLIDO_VICTIMA VARCHAR2(150),
+            DIRECCION_VICTIMA VARCHAR2(150),
+            FECHA_PRIMERA_SOSPECHA DATE,
+            FECHA_CONFIRMACION DATE,
+            FECHA_MUERTE DATE,
+            ESTADO_VICTIMA VARCHAR2(100),
+            NOMBRE_ASOCIADO VARCHAR2(150),
+            APELLIDO_ASOCIADO VARCHAR2(150),
+            FECHA_CONOCIO DATE,
+            CONTACTO_FISICO  VARCHAR2(100),
+            FECHA_INICIO_CONTACTO DATE,
+            FECHA_FIN_CONTACTO DATE,
+            NOMBRE_HOSPITAL VARCHAR2(150),
+            DIRECCION_HOSPITAL VARCHAR2(150),
+            UBICACION_VICTIMA VARCHAR2(150),
+            FECHA_LLEGADA DATE,
+            FECHA_RETIRO DATE,
+            TRATAMIENTO VARCHAR(100),
+            EFECTIVIDAD NUMBER,
+            FECHA_INICIO_TRATAMIENTO DATE,
+            FECHA_FIN_TRATAMIENTO DATE,
+            EFECTIVIDAD_EN_VICTIMA NUMBER
+        )`);
+        
+        await connection.close()
+        res.status(200).send({msg:"Se eliminó correctamente los datos de la temporal", valid:true})
     }catch(error){
-        res.status(400).send({msg:"error en server"})
+        res.status(400).send({msg:"error en server", err:error})
     }
 }
 
 exports.deleteMod = async function(req,res){
     try{
-        res.status(200).send({msg:"eliminarModelo", valid:true})
+        var connection = await oracledb.getConnection({
+            user: user,
+            password: pass,
+            connectString: conn
+        });
+
+        await connection.execute('DROP TABLE victima_ubicacion');
+        await connection.execute('DROP TABLE victima_tratamiento');
+        await connection.execute('DROP TABLE victima_hospital');
+        await connection.execute('DROP TABLE victima_asociado');
+        await connection.execute('DROP TABLE tratamiento');
+        await connection.execute('DROP TABLE hospital');
+        await connection.execute('DROP TABLE contacto');
+        await connection.execute('DROP TABLE asociado');
+        await connection.execute('DROP TABLE ubicacion');
+        await connection.execute('DROP TABLE victimas_virus');
+        await connection.close()
+
+        res.status(200).send({msg:"Tablas eliminadas correctamente", valid:true})
     }catch(error){
-        res.status(400).send({msg:"error en server"})
+        console.log("%%%%")
+        console.log(error)
+        console.log("%%%%")
+        res.status(400).send({msg:"error en server", err:error})
     }
 }
 
@@ -158,28 +223,167 @@ exports.chargeTemp = async function(req,res){
 }
 
 exports.chargeModel = async function(req,res){
-    try {
+    try{
         var connection = await oracledb.getConnection({
-          user: user,
-          password: pass,
-          connectString: conn
-      });
+            user: user,
+            password: pass,
+            connectString: conn
+        });
+        // CREO MI TABLA VICTIMA_VIRUS
+        await connection.execute(`
+        CREATE TABLE victimas_virus (
+            id_victima        		NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            nombre_victima    		VARCHAR2(50) NULL,
+            apellido_victima  		VARCHAR2(50) NULL,
+            direccion_victima 		VARCHAR2(200) NULL,
+            fecha_primera_sospecha  DATE NULL,
+            fecha_confirmacion		DATE NULL,
+            fecha_muerte			DATE NULL,
+            estado_victima			VARCHAR2(50) NULL
+        )`);
+        // CREO MI TABLA TRATAMIENTO
+        await connection.execute(`
+        CREATE TABLE tratamiento (
+            id_tratamiento            NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            tratamiento               VARCHAR2(150) NULL,
+            efectividad               INTEGER NULL
+        )`);
+        // CREO MI TABLA HOSPITAL
+        await connection.execute(`
+        CREATE TABLE hospital (
+            id_hospital             NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            NOMBRE               	VARCHAR2(150) NULL,
+            direccion               VARCHAR2(150) NULL
+        )`)
+        // CREO MI TABLA CONTACTO
+        await connection.execute(`
+        CREATE TABLE contacto (
+            id_contacto             NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            TIPO 		          	VARCHAR2(150) NULL   
+        )`)
+        // CREO MI TABLA UBICACION
+        await connection.execute(`
+        CREATE TABLE ubicacion (
+            id_ubicacion             NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            DIRECCION	          	 VARCHAR2(150) NULL
+        )`)
+        // CREO MI TABLA ASOCIADO
+        await connection.execute(`
+        CREATE TABLE asociado(
+            id_asociado 			NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            nombre_asociado    		VARCHAR2(50) NULL,
+            apellido_asociado  		VARCHAR2(50) NULL
+        )`)
+        // CREO MI TABLA VICTIMA_TRATAMIENTO
+        await connection.execute(`
+        CREATE TABLE victima_tratamiento(
+            id_victima_tratamiento NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            efectividad_vic		   INTEGER NULL,
+            fecha_ini_trat		   DATE NULL,
+            fecha_fin_trat		   DATE NULL,
+            id_victima 			   NUMBER(10),
+            id_tratamiento 		   NUMBER(10),
+            FOREIGN KEY (id_victima) REFERENCES victimas_virus(id_victima),
+            FOREIGN KEY (id_tratamiento) REFERENCES tratamiento(id_tratamiento)
+        )`)
+        //CREO MI TABLA VICTIMA_HOSPITAL
+        await connection.execute(`
+        CREATE TABLE victima_hospital(
+            id_victima_hospital NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            fecha_llegada		   DATE,
+            fecha_retiro		   DATE,
+            id_victima 			   NUMBER(10),
+            id_hospital 		   NUMBER(10),
+            FOREIGN KEY (id_victima) REFERENCES victimas_virus(id_victima),
+            FOREIGN KEY (id_hospital) REFERENCES hospital(id_hospital)
+        )`)
+        //CREO MI TABLA VICTIMA_ASOCIADO
+        await connection.execute(`
+        CREATE TABLE victima_asociado(
+            id_victima_asociado 	NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            fecha_conocio		DATE,
+            fecha_ini_cont 		DATE,
+            fecha_fin_cont		DATE,
+            id_victima    		NUMBER(10) NULL,
+            id_asociado  		NUMBER(10) NULL,
+            id_contacto			NUMBER(10) NULL,
+            FOREIGN KEY (id_victima) REFERENCES victimas_virus(id_victima),
+            FOREIGN KEY (id_asociado) REFERENCES asociado(id_asociado),
+            FOREIGN KEY (id_contacto) REFERENCES contacto(id_contacto)
+        )`)
+        //CREO MI TABLA VICTIMA_UBICACION
+        await connection.execute(`
+        CREATE TABLE victima_ubicacion(
+            id_victima_ubicacion 	NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            id_victima    			NUMBER(10) NULL,
+            id_ubicacion  			NUMBER(10) NULL,
+            FOREIGN KEY (id_victima) REFERENCES victimas_virus(id_victima),
+            FOREIGN KEY (id_ubicacion ) REFERENCES ubicacion(id_ubicacion )
+        )`)
+        //LLENO MI TABLA DE VICTIMAS_VIRUS
+        await connection.execute(`
+        INSERT INTO victimas_virus (NOMBRE_VICTIMA, APELLIDO_VICTIMA, DIRECCION_VICTIMA, FECHA_PRIMERA_SOSPECHA, FECHA_CONFIRMACION, FECHA_MUERTE, ESTADO_VICTIMA)
+        SELECT DISTINCT t.NOMBRE_VICTIMA , t.APELLIDO_VICTIMA ,t.DIRECCION_VICTIMA ,t.FECHA_PRIMERA_SOSPECHA , t.FECHA_CONFIRMACION , t.FECHA_MUERTE , t.ESTADO_VICTIMA 
+        FROM TEMPORAL t WHERE t.NOMBRE_VICTIMA IS NOT NULL AND t.APELLIDO_VICTIMA  IS NOT NULL`)
+        //LLENO MI TABLA DE TRATAMIENTOS
+        await connection.execute(`
+        INSERT INTO TRATAMIENTO (TRATAMIENTO, EFECTIVIDAD)
+        SELECT DISTINCT t.TRATAMIENTO , t.EFECTIVIDAD 
+        FROM TEMPORAL t WHERE T.TRATAMIENTO IS NOT NULL`)
+        //LLENO MI TABLA DE CONTACTOS
+        await connection.execute(`
+        INSERT INTO CONTACTO (CONTACTO.TIPO)
+        SELECT DISTINCT T.CONTACTO_FISICO FROM TEMPORAL t 
+        WHERE T.CONTACTO_FISICO IS NOT NULL`)
+        //LLENO MI TABLA DE HOSPITAL
+        await connection.execute(`
+        INSERT INTO HOSPITAL (NOMBRE,DIRECCION)
+        SELECT DISTINCT t.NOMBRE_HOSPITAL, t.DIRECCION_HOSPITAL  
+        FROM TEMPORAL t WHERE t.NOMBRE_HOSPITAL IS NOT NULL`)
+        //LLENO MI TABLA DE ASOCIADOS
+        await connection.execute(`
+        INSERT INTO ASOCIADO a (a.NOMBRE_ASOCIADO,a.APELLIDO_ASOCIADO)
+        SELECT DISTINCT t.NOMBRE_ASOCIADO, t.APELLIDO_ASOCIADO FROM TEMPORAL t
+        WHERE t.NOMBRE_ASOCIADO IS NOT NULL AND t.APELLIDO_ASOCIADO IS NOT NULL`)
+        //LLENO MI TABLA DE UBICACION
+        await connection.execute(`
+        INSERT INTO UBICACION (UBICACION.DIRECCION)
+        SELECT DISTINCT T.UBICACION_VICTIMA FROM TEMPORAL t  WHERE T.UBICACION_VICTIMA IS NOT NULL`)
+        //LLENO MI TABLA VICTIMA_TRATAMIENTO
+        await connection.execute(`
+        INSERT INTO VICTIMA_TRATAMIENTO (efectividad_vic,fecha_ini_trat	,fecha_fin_trat,id_victima,id_tratamiento)
+        SELECT DISTINCT T.EFECTIVIDAD_EN_VICTIMA , T.FECHA_INICIO_TRATAMIENTO, T.FECHA_FIN_TRATAMIENTO, VV.ID_VICTIMA , T2.ID_TRATAMIENTO   FROM TEMPORAL t 
+        JOIN VICTIMAS_VIRUS vv ON T.NOMBRE_VICTIMA = VV.NOMBRE_VICTIMA  AND T.APELLIDO_VICTIMA = VV.APELLIDO_VICTIMA
+        JOIN TRATAMIENTO t2 ON T2.TRATAMIENTO  = T.TRATAMIENTO`)
+        //LLENO MI TABLA VICTIMA_HOSPITAL
+        await connection.execute(`
+        INSERT INTO VICTIMA_HOSPITAL (FECHA_LLEGADA, FECHA_RETIRO,ID_VICTIMA,ID_HOSPITAL)
+        SELECT DISTINCT T.FECHA_LLEGADA ,T.FECHA_RETIRO, VV.ID_VICTIMA , H.ID_HOSPITAL   FROM TEMPORAL t 
+        JOIN VICTIMAS_VIRUS vv ON T.NOMBRE_VICTIMA = VV.NOMBRE_VICTIMA  AND T.APELLIDO_VICTIMA = VV.APELLIDO_VICTIMA
+        JOIN HOSPITAL h ON T.NOMBRE_HOSPITAL = H.NOMBRE 
+        WHERE T.FECHA_LLEGADA IS NOT NULL AND T.FECHA_RETIRO IS NOT NULL`)
+        //LLENO MI TABLA VICTIMA_ASOCIADO
+        await connection.execute(`
+        INSERT INTO VICTIMA_ASOCIADO va (va.ID_VICTIMA,va.ID_ASOCIADO, va.FECHA_CONOCIO, va.FECHA_INI_CONT, va.FECHA_FIN_CONT)
+        SELECT DISTINCT VV.ID_VICTIMA , A.ID_ASOCIADO , T.FECHA_CONOCIO, T.FECHA_INICIO_CONTACTO , T.FECHA_FIN_CONTACTO   FROM TEMPORAL t 
+        JOIN ASOCIADO a ON T.NOMBRE_ASOCIADO = A.NOMBRE_ASOCIADO AND T.APELLIDO_ASOCIADO = A.APELLIDO_ASOCIADO 
+        JOIN VICTIMAS_VIRUS vv ON T.NOMBRE_VICTIMA = VV.NOMBRE_VICTIMA  AND T.APELLIDO_VICTIMA = VV.APELLIDO_VICTIMA
+        JOIN CONTACTO c ON C.TIPO = T.CONTACTO_FISICO 
+        WHERE t.FECHA_CONOCIO IS NOT NULL  AND t.FECHA_INICIO_CONTACTO  IS NOT NULL AND t.FECHA_FIN_CONTACTO IS NOT NULL`)
+        //LLENO MI TABLA VICTIMA_UBICACION
+        await connection.execute(`
+        INSERT INTO VICTIMA_UBICACION vu (vu.ID_VICTIMA,vu.ID_UBICACION)
+        SELECT DISTINCT vv.ID_VICTIMA, u.ID_UBICACION  FROM TEMPORAL t 
+        JOIN VICTIMAS_VIRUS vv ON T.NOMBRE_VICTIMA = VV.NOMBRE_VICTIMA  AND T.APELLIDO_VICTIMA = VV.APELLIDO_VICTIMA
+        JOIN UBICACION u ON u.DIRECCION = t.UBICACION_VICTIMA`)
 
-      const sql = fs.readFileSync('./archivos_sql/archivo_sql_datos.sql' , 'utf-8');
-      
-      const statements = sql.split(';')
-      console.log(statements)
-      connection.executeMany(statements,{},(err,result)=>{
-        if(err){
-            console.error(err.message);
-        }else{
-            console.log('Ejecución exitosa de todas las sentencias');
-        }
-      })
-      await connection.close();
-      console.log('El archivo SQL se ha ejecutado correctamente');
+        await connection.close()
 
-      }catch(error){
-        console.error(error);
-      }
+        res.status(200).send({msg:"Tablas creadas y modelo correctamente", valid:true})
+    }catch(error){
+        console.log("%%%%")
+        console.log(error)
+        console.log("%%%%")
+        res.status(400).send({msg:"error en server", err:error})
+    }
 }
